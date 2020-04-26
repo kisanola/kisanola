070afd9dcae6296e4c4ba0c8490ab72d4c40213b
Olivier ESUKA <oesukam@gmail.com>
2020-04-26 19:41:33 +0200

    add welcome page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import logoImage from './assets/images/kisanola.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <img src={logoImage} className="app-logo" alt="logo" />
+      <p>
+        244devs
+      </p>
     </div>
   );
 }
